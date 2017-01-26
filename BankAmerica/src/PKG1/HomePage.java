2e1b8bbb64e3b2f673d7ec53c9222727e9bd1901
yasin <yasin@Yasin>
2017-01-26 18:21:31 -0500

    Project Added

---
+++ BankAmerica/src/PKG1/HomePage.java
@@ -0,0 +1,11 @@
+package PKG1;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Bank of America");
+
+	}
+
+}
